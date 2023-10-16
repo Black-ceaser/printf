6171c5bc167f152c90e0c33e0ceec1c6c29dce01
Black-ceaser <calvin.annan@gmail.com>
2023-10-16 14:31:54 -0700

    Created printf file

--- _printf.c
+++ _printf.c
@@ -7,21 +7,44 @@
  */
 int _printf(const char *format, ...)
 {
-	int charprint = 0;
+	int i;
+	int prntd = 0;
+	int prntd_chars = 0;
+	int flags = 0;
+	int width = 0;
+	int precision = 0;
+	int size = 0;
+	int buff_index = 0;
 
 	va_list ls_of_args;
+	char buffer[BUFF_SIZE];
 
-	va_start;
+	if (format == NULL)
+		return (-1);
 
+	va_start(ls_of_args, format);
 
-	va_arg;
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		buffer[buffer_index++] = format[i];
+		if (buff_index == BUFF_SIZE)
+			print_buffer(buffer, &buff_index);
+		prntd_chars++;
+	}
+	else
+	{
+		print_buffer(buffer, &buff_index);
+		flags = get_flags(format, &i);
+		width = get_width(format, &i, ls_of_args);
+		precision = get_precision(format, &i, ls_of_args);
+		size = get_size(formt, &i);
+		++i;
+		prntd = handle_print(format, &i, ls_of_args, buffer, flags,
+				width, precision, size);
+	}
+}
+print_buffer(buffer, &buff_index);
 
-
-
-
-
-
-
-
-
+va_end(ls_of_args);
+return (printed_chars);
 }
